11f59eaa398d70e1444151414cc5235ce68584d9
Alan D. Tse <alandtse@gmail.com>
2021-02-01 20:53:54 -0800

    fix: address attribute error properly for form

--- teslajsonpy/connection.py
+++ teslajsonpy/connection.py
@@ -436,7 +436,7 @@ def get_inputs(soup: BeautifulSoup, searchfield=None) -> Dict[str, str]:
     form = soup.find("form", searchfield)
     if not form:
         form = soup.find("form")
-    if form.find_all("input"):
+    if form:
         for field in form.find_all("input"):
             try:
                 data[field["name"]] = ""
