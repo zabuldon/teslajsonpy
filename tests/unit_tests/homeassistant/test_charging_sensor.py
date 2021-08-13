3e3d1a384eb19cf13c07d78c2c46ea747c0c9be3
Alan D. Tse <alandtse@gmail.com>
2021-08-13 00:15:24 -0700

    feat: add ChargingEnergySensor

--- tests/unit_tests/homeassistant/test_charging_sensor.py
+++ tests/unit_tests/homeassistant/test_charging_sensor.py
@@ -3,7 +3,7 @@
 import pytest
 
 from teslajsonpy.controller import Controller
-from teslajsonpy.homeassistant.charger import ChargingSensor
+from teslajsonpy.homeassistant.charger import ChargingSensor, ChargingEnergySensor
 
 from tests.tesla_mock import TeslaMock
 
@@ -32,8 +32,8 @@ def test_device_class(monkeypatch):
     assert _sensor.device_class is None
 
 
-def test_get_value_on_init(monkeypatch):
-    """Test get_value() after initialization."""
+def test_state_class(monkeypatch):
+    """Test device_class()."""
 
     _mock = TeslaMock(monkeypatch)
     _controller = Controller(None)
@@ -41,15 +41,36 @@ def test_get_value_on_init(monkeypatch):
     _data = _mock.data_request_vehicle()
     _sensor = ChargingSensor(_data, _controller)
 
-    assert not _sensor is None
-    assert _sensor.charging_rate is None
-    assert _sensor.time_left is None
-    assert _sensor.added_range is None
-    assert _sensor.charge_current_request is None
-    assert _sensor.charger_actual_current is None
-    assert _sensor.charger_voltage is None
-    assert _sensor.charge_energy_added is None
-    assert _sensor.charge_limit_soc is None
+    assert _sensor.device_class is None
+
+    _sensor2 = ChargingEnergySensor(_data, _controller)
+    assert _sensor2.device_class == "energy"
+
+
+def test_get_value_on_init(monkeypatch):
+    """Test get_value() after initialization."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+
+    _data = _mock.data_request_vehicle()
+    sensors = [
+        ChargingSensor(_data, _controller),
+        ChargingEnergySensor(_data, _controller),
+    ]
+
+    for _sensor in sensors:
+
+        assert _sensor is not None
+        assert _sensor.charging_rate is None
+        assert _sensor.time_left is None
+        assert _sensor.added_range is None
+        assert _sensor.charge_current_request is None
+        assert _sensor.charger_actual_current is None
+        assert _sensor.charger_voltage is None
+        assert _sensor.charger_power is None
+        assert _sensor.charge_energy_added is None
+        assert _sensor.charge_limit_soc is None
 
 
 @pytest.mark.asyncio
@@ -61,7 +82,7 @@ async def test_get_value_after_update(monkeypatch):
 
     _data = _mock.data_request_vehicle()
     _sensor = ChargingSensor(_data, _controller)
-
+    _sensor2 = ChargingEnergySensor(_data, _controller)
     await _sensor.async_update()
 
     assert _sensor is not None
@@ -74,7 +95,23 @@ async def test_get_value_after_update(monkeypatch):
     assert _sensor.charger_power == 0
     assert _sensor.charge_energy_added == 12.41
     assert _sensor.charge_limit_soc == 90
+    assert _sensor.device_class is None
 
+    _sensor2 = ChargingEnergySensor(_data, _controller)
+    await _sensor2.async_update()
+
+    assert _sensor2 is not None
+    assert _sensor2.charging_rate == 0
+    assert _sensor2.time_left == 0
+    assert _sensor2.charge_current_request == 48
+    assert _sensor2.charger_actual_current == 0
+    assert _sensor2.charger_voltage == 0
+    assert _sensor2.charger_power == 0
+    assert _sensor2.charge_energy_added == 12.41
+    assert _sensor2.charge_limit_soc == 90
+    assert _sensor2.last_reset != 0
+    assert _sensor2.state_class == "measurement"
+    assert _sensor2.device_class == "energy"
 
 @pytest.mark.asyncio
 async def test_async_update(monkeypatch):
