7043e4cf8368f2742b5475ee6efea868c7b9e9f8
Giovanni Iachello <60585229+giachello@users.noreply.github.com>
2021-10-02 16:57:46 -0700

    Add Support for Energy Sites

---
+++ teslajsonpy/homeassistant/power.py
@@ -0,0 +1,186 @@
+"""
+Python Package for controlling Tesla API.
+
+
+"""
+import time
+import teslajsonpy
+
+from typing import List, Optional, Text
+
+from teslajsonpy.exceptions import UnknownPresetMode
+
+"""
+Python Package for controlling Tesla API.
+
+For more details about this api, please refer to the documentation at
+https://github.com/zabuldon/teslajsonpy
+"""
+import logging
+from typing import Dict, Optional, Text
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class EnergySiteDevice:
+    """Home-assistant class of Tesla Energy Sites.
+
+    This is intended to be partially inherited by a Home-Assitant entity.
+    """
+
+    def __init__(self, data, controller):
+        """Initialize the Energy Site.
+
+        Parameters
+        ----------
+        data : dict
+            The base state for a Tesla vehicle.
+            https://tesla-api.timdorr.com/vehicle/state/data
+        controller : teslajsonpy.Controller
+            The controller that controls updates to the Tesla API.
+
+        Returns
+        -------
+        None
+
+        """
+        self._id: int = data["id"]
+        self._energy_site_id: int = data["energy_site_id"]
+        self._display_name: Text = data["site_name"]
+        self._solar_type: Text = data["solar_type"]
+        self._controller = controller
+        self.should_poll: bool = True
+        self.type: Text = "device"
+        self.attrs: Dict[Text, Text] = {}
+
+    def _name(self) -> Text:
+        return "{} {}".format(self._display_name, self.type)
+
+    def _uniq_name(self) -> Text:
+        return self._name()
+
+    def id(self) -> int:
+        # pylint: disable=invalid-name
+        """Return the id of this Vehicle."""
+        return self._id
+
+    def energy_site_id(self) -> int:
+        """Return the vehicle_id of this Vehicle."""
+        return self._energy_site_id
+
+    def site_name(self) -> Text:
+        """Return the car name of this Vehicle."""
+        return self._display_name
+
+    @property
+    def solar_type(self) -> Text:
+        """Return the type of this Vehicle."""
+        return self._solar_type
+
+    async def async_update(
+        self, wake_if_asleep: bool = False, force: bool = False
+    ) -> None:
+        """Update the vehicle data.
+
+        This function will call a controller update.
+        """
+        await self._controller.update(
+            self.id(), wake_if_asleep=wake_if_asleep, force=force
+        )
+        self.refresh()
+
+    def refresh(self) -> None:
+        """Refresh the vehicle data.
+
+        This assumes the controller has already been updated. This should be
+        called by inherited classes so the overall vehicle information is updated.
+        """
+        # state = self._controller.get_state_params(self.id())
+        # if state and "car_version" in state:
+        #     self._car_version = state["car_version"]
+        # if state and "sentry_mode_available" in state:
+        #     self._sentry_mode_available = state["sentry_mode_available"]
+        # self._update_available = state.get("software_update", {}).get("status") in {
+        #     "available",
+        #     "scheduled",
+        # }
+        # self._update_version = state.get("software_update", {}).get("version")
+
+
+class PowerSensor(EnergySiteDevice):
+    """Home-assistant class of temperature sensors for Tesla vehicles.
+
+    This is intended to be partially inherited by a Home-Assitant entity.
+    """
+
+    def __init__(self, data, controller):
+        """Initialize the temperature sensors and track in celsius.
+
+        Vehicles have both a driver and passenger.
+
+        Parameters
+        ----------
+        data : dict
+            The base state for a Tesla vehicle.
+            https://tesla-api.timdorr.com/vehicle/state/data
+        controller : teslajsonpy.Controller
+            The controller that controls updates to the Tesla API.
+
+        Returns
+        -------
+        None
+
+        """
+        super().__init__(data, controller)
+        self.__power: float = data["solar_power"]
+        self.__generating_status: bool = None
+        self.type = "solar panel"
+        self.measurement = "W"
+        self.hass_type = "sensor"
+        self._device_class: Text = "power"
+        self.name = self._name()
+        self.uniq_name = self._uniq_name()
+        self.bin_type = 0x4
+
+    async def async_update(self, wake_if_asleep=False, force=False) -> None:
+        """Update the temperature."""
+        await super().async_update(wake_if_asleep=wake_if_asleep)
+        self.refresh()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        data = self._controller.get_power_params(self._id)
+        if data:
+            if "grid_status" in data and data["grid_status"] == "Unknown":
+                return
+            else:
+                self.__power = data["solar_power"]
+                self.__generating_status = (
+                    "Generating" if data["solar_power"] > 0 else "Idle"
+                )
+
+    # @staticmethod
+    # def has_battery():
+    #     """Return whether the device has a battery."""
+    #     return False
+
+    def get_value(self) -> float:
+        """Return the battery level."""
+        return self.__power
+
+    def get_power(self):
+        """Get inside temperature."""
+        return self.__power
+
+    def get_generating_status(self):
+        """Get outside temperature."""
+        return self.__generating_status
+
+    @property
+    def device_class(self) -> Text:
+        """Return the HA device class."""
+        return self._device_class
