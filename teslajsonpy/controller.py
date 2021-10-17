f9d3c1acf35b8a1dad4311d7ed33a8855dcb433c
Giovanni Iachello <giovanni.iachello@gmail.com>
2021-10-17 09:13:05 -0700

    Site name handling to use get.

--- teslajsonpy/controller.py
+++ teslajsonpy/controller.py
@@ -48,7 +48,7 @@ from teslajsonpy.homeassistant.lock import ChargerLock, Lock
 from teslajsonpy.homeassistant.sentry_mode import SentryModeSwitch
 from teslajsonpy.homeassistant.trunk import FrunkLock, TrunkLock
 from teslajsonpy.homeassistant.heated_steering_wheel import HeatedSteeringWheelSwitch
-from teslajsonpy.homeassistant.power import PowerSensor
+from teslajsonpy.homeassistant.power import PowerSensor, TESLA_DEFAULT_ENEREGY_SITE_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -346,10 +346,7 @@ class Controller:
             energysite_id = energysite["energy_site_id"]
             self.__id_energysiteid_map[energysite["id"]] = energysite_id
             self.__energysiteid_id_map[energysite_id] = energysite["id"]
-            if "site_name" in energysite:
-                self.__energysite_name[energysite_id] = energysite["site_name"]
-            else:
-                self.__energysite_name[energysite_id] = "My Home"
+            self.__energysite_name[energysite_id] = energysite.get("site_name", TESLA_DEFAULT_ENEREGY_SITE_NAME)
             self.__energysite_type[energysite_id] = energysite["solar_type"]
             self.__power[energysite_id] = {"solar_power": energysite["solar_power"]}
             self.__lock[energysite_id] = asyncio.Lock()
