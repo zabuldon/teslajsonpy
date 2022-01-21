844a396ef06c7e1be1f6c5cf93c74b44b6c66262
ehendrix23 <hendrix_erik@hotmail.com>
2022-01-20 23:37:05 -0700

    Fix lint issues

--- teslajsonpy/homeassistant/binary_sensor.py
+++ teslajsonpy/homeassistant/binary_sensor.py
@@ -217,11 +217,11 @@ class OnlineSensor(BinarySensor):
         self.attrs["update_interval"] = self._controller.update_interval
         vehicle_data = {
             "climate_state": self._controller.get_climate_params(self._id),
-            "charge_state": self._controller.get_charging_params(self._id), 
-            "vehicle_state":  self._controller.get_state_params(self._id),
+            "charge_state": self._controller.get_charging_params(self._id),
+            "vehicle_state": self._controller.get_state_params(self._id),
             "vehicle_config": self._controller.get_config_params(self._id),
             "drive_state": self._controller.get_drive_params(self._id),
-            "gui_settings": self._controller.get_gui_params(self._id)            
+            "gui_settings": self._controller.get_gui_params(self._id)
         }
         self.attrs["vehicle_data"] = dumps(vehicle_data)
 
