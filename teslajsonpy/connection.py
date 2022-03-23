d65f988415e50640981a135e63e02b356e5310da
Alan D. Tse <alandtse@gmail.com>
2022-03-22 22:47:54 -0700

    style: fix typing

--- teslajsonpy/connection.py
+++ teslajsonpy/connection.py
@@ -49,7 +49,7 @@ class Connection:
         refresh_token: Text = None,
         authorization_token: Text = None,
         expiration: int = 0,
-        auth_domain: str = AUTH_DOMAIN
+        auth_domain: str = AUTH_DOMAIN,
     ) -> None:
         """Initialize connection object."""
         self.user_agent: Text = "Model S 2.1.79 (SM-G900V; Android REL 4.4.4; en_US"
