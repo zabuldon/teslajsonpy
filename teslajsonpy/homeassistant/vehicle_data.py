zabuldon/teslajsonpy

commit d692a154dd4165b3f24ba29abd6f76a78858e0b7

fix!: create json sensors for vehicle data (#299)
Remove json attributes from online sensor and move to separate sensors.

BREAKING CHANGE: Online sensor will no longer have json vehicle data. Any scripts that relied on that json data will need to use the newer sensors. They will need to be enabled.

closes #298

teslajsonpy/homeassistant/vehicle_data.py
@@ -1,0 +1,317 @@
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Python Package for controlling Tesla API.
+
+For more details about this api, please refer to the documentation at
+https://github.com/zabuldon/teslajsonpy
+"""
+import logging
+
+from typing import Optional, Text
+from teslajsonpy.homeassistant.vehicle import VehicleDevice
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class VehicleDataSensor(VehicleDevice):
+    """Home-Assistant vehicle data class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the vehicle data sensor.
+
+        Parameters
+        ----------
+        data : dict
+            The base state for a Tesla vehicle.
+            https://tesla-api.timdorr.com/vehicle/state/data
+        controller : teslajsonpy.Controller
+            The controller that controls updates to the Tesla API.
+
+        Returns
+        -------
+        None
+
+        """
+        super().__init__(data, controller)
+        self.__state: Optional[str] = None
+
+        self.type: Text = "sensor"
+        self.hass_type: Text = "sensor"
+        # this will be returned to HA as a device_class
+        # https://developers.home-assistant.io/docs/core/entity/binary-sensor
+        self._sensor_type: Optional[Text] = None
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+        self._device_class: Optional[Text] = None
+        self.measurement: Optional[Text] = None
+        self.enabled_by_default = False
+
+    async def async_update(self, wake_if_asleep=False, force=False) -> None:
+        """Update the vehicle data."""
+        await super().async_update(wake_if_asleep=wake_if_asleep)
+        self.refresh()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.__state = self.vin()
+
+    def get_value(self) -> Optional[str]:
+        """Return the state."""
+        return self.__state
+
+    @property
+    def device_class(self) -> Optional[Text]:
+        """Return the HA device class."""
+        return self._device_class
+
+    @classmethod
+    def _dict_to_attr(
+        cls,
+        data: dict,
+        exclude_dicts: Optional[list[str]] = None,
+        prepend: Optional[str] = None,
+    ) -> dict:
+        """Convert Tesla returned dict into dict for attributes."""
+        prepend = prepend or ""
+        exclude_dicts = exclude_dicts or []
+
+        attr: dict = {}
+        for key, value in data.items():
+            prepend_key = f"{key}" if prepend == "" else f"{prepend}_{key}"
+            if isinstance(value, dict):
+                if key in exclude_dicts or "*" in exclude_dicts:
+                    continue
+                attr.update(cls._dict_to_attr(value, exclude_dicts, prepend_key))
+            else:
+
+                attr[prepend_key] = value
+
+        return attr
+
+
+class ClimateStateDataSensor(VehicleDataSensor):
+    """Home-Assistant Climate State sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the ClimateStateData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "climate state data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+
+        self.attrs = self._dict_to_attr(self._controller.get_climate_params(self._id))
+
+
+class ChargeStateDataSensor(VehicleDataSensor):
+    """Home-Assistant Charge State sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the ChargeStateData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "charging state data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(self._controller.get_charging_params(self._id))
+
+
+class VehicleStateDataSensor(VehicleDataSensor):
+    """Home-Assistant Vehicle State sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the VehicleStateData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "vehicle state data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(
+            self._controller.get_state_params(self._id),
+            ["software_update", "speed_limit_mode"],
+        )
+
+
+class SoftwareDataSensor(VehicleDataSensor):
+    """Home-Assistant Software sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the SoftwareData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "software data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(
+            self._controller.get_state_params(self._id).get("software_update", {})
+        )
+
+
+class SpeedLimitDataSensor(VehicleDataSensor):
+    """Home-Assistant Speed Limit sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the SpeedLimitData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "speed limit data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(
+            self._controller.get_state_params(self._id).get("speed_limit_mode", {})
+        )
+
+
+class VehicleConfigDataSensor(VehicleDataSensor):
+    """Home-Assistant Vehicle Config sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the VehicleConfigData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "vehicle config data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(self._controller.get_config_params(self._id))
+
+
+class DriveStateDataSensor(VehicleDataSensor):
+    """Home-Assistant Drive State sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the DriveStateData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "drive state data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(self._controller.get_drive_params(self._id))
+
+
+class GuiSettingsDataSensor(VehicleDataSensor):
+    """Home-Assistant GUI settings sensor class for a Tesla VehicleDevice."""
+
+    def __init__(self, data: dict, controller) -> None:
+        """Initialize the GuiSettingsData sensor.
+
+        Args:
+            data (Dict): The base state for a Tesla vehicle.
+                https://tesla-api.timdorr.com/vehicle/state/data
+            controller (Controller): The controller that controls updates to the Tesla API.
+
+        """
+        super().__init__(data, controller)
+
+        self.type: Text = "gui settings data sensor"
+        self.name: Text = self._name()
+        self.uniq_name: Text = self._uniq_name()
+
+    def refresh(self) -> None:
+        """Refresh data.
+
+        This assumes the controller has already been updated
+        """
+        super().refresh()
+        self.attrs = self._dict_to_attr(self._controller.get_gui_params(self._id))

teslajsonpy/controller.py
@@ -54,6 +54,17 @@ from teslajsonpy.homeassistant.trunk import FrunkLock, TrunkLock
 from teslajsonpy.homeassistant.heated_steering_wheel import HeatedSteeringWheelSwitch
 from teslajsonpy.homeassistant.power import PowerSensor
 from teslajsonpy.homeassistant.alerts import Horn, FlashLights
+from teslajsonpy.homeassistant.vehicle_data import (
+    ChargeStateDataSensor,
+    ClimateStateDataSensor,
+    DriveStateDataSensor,
+    GuiSettingsDataSensor,
+    SoftwareDataSensor,
+    SpeedLimitDataSensor,
+    VehicleConfigDataSensor,
+    VehicleDataSensor,
+    VehicleStateDataSensor,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -724,6 +735,17 @@ class Controller:
         self.__components.append(HeatedSteeringWheelSwitch(car, self))
         self.__components.append(Horn(car, self))
         self.__components.append(FlashLights(car, self))
+        self.__components.append(ChargeStateDataSensor(car, self))
+        self.__components.append(ClimateStateDataSensor(car, self))
+        self.__components.append(DriveStateDataSensor(car, self))
+        self.__components.append(GuiSettingsDataSensor(car, self))
+        self.__components.append(SoftwareDataSensor(car, self))
+        self.__components.append(SpeedLimitDataSensor(car, self))
+        self.__components.append(VehicleConfigDataSensor(car, self))
+        self.__components.append(VehicleDataSensor(car, self))
+        self.__components.append(VehicleStateDataSensor(car, self))
+        self.__components.append(ChargeStateDataSensor(car, self))
+
         for seat in ["left", "right", "rear_left", "rear_center", "rear_right"]:
             try:
                 self.__components.append(HeatedSeatSelect(car, self, seat))

teslajsonpy/homeassistant/binary_sensor.py
@@ -5,7 +5,6 @@ Python Package for controlling Tesla API.
 For more details about this api, please refer to the documentation at
 https://github.com/zabuldon/teslajsonpy
 """
-from json import dumps
 from typing import Dict, Optional, Text
 
 from teslajsonpy.const import RELEASE_NOTES_URL
@@ -217,15 +216,6 @@ class OnlineSensor(BinarySensor):
         self.attrs["update_interval"] = self._controller.get_update_interval_vin(
             vin=self._vin
         )
-        vehicle_data = {
-            "climate_state": self._controller.get_climate_params(self._id),
-            "charge_state": self._controller.get_charging_params(self._id),
-            "vehicle_state": self._controller.get_state_params(self._id),
-            "vehicle_config": self._controller.get_config_params(self._id),
-            "drive_state": self._controller.get_drive_params(self._id),
-            "gui_settings": self._controller.get_gui_params(self._id),
-        }
-        self.attrs["vehicle_data"] = dumps(vehicle_data)
 
     def get_value(self) -> Optional[bool]:
         """Return the car is online."""

teslajsonpy/homeassistant/vehicle.py
@@ -55,6 +55,7 @@ class VehicleDevice:
         self._update_version: Optional[Text] = data.get("software_update", {}).get(
             "version"
         )
+        self.enabled_by_default: bool = True
 
     def _name(self) -> Text:
         return (

tests/unit_tests/homeassistant/test_online_sensor.py
@@ -78,9 +78,6 @@ async def test_get_value_on(monkeypatch):
         "id": 12345678901234567,
         "vehicle_id": 1234567890,
         "update_interval": 300,
-        'vehicle_data': '{"climate_state": {}, "charge_state": {}, "vehicle_state": '
-                        '{}, "vehicle_config": {}, "drive_state": {}, "gui_settings": '
-                        '{}}'
     }
 
 
@@ -96,7 +93,7 @@ async def test_get_value_off(monkeypatch):
     _data = _mock.data_request_vehicle()
     _sensor = OnlineSensor(_data, _controller)
     _data["state"] = "asleep"
-    
+
     await _sensor.async_update()
 
     assert _sensor is not None
@@ -108,46 +105,4 @@ async def test_get_value_off(monkeypatch):
         "id": 12345678901234567,
         "vehicle_id": 1234567890,
         "update_interval": 300,
-        'vehicle_data': '{"climate_state": {}, "charge_state": {}, "vehicle_state": '
-                        '{}, "vehicle_config": {}, "drive_state": {}, "gui_settings": '
-                        '{}}'
     }
-
-@pytest.mark.asyncio
-async def test_get_vehicle_data_attribute(monkeypatch):
-    """Test get_value() for offline mode."""
-
-    _mock = TeslaMock(monkeypatch)
-    _controller = Controller(None)
-    _controller.set_id_vin(CAR_ID, VIN)
-    monkeypatch.setitem(_controller.car_online, VIN, False)
-    monkeypatch.setitem(_controller.car_state, VIN, _mock.data_request_vehicle())
-
-    _data = _mock.data_request_vehicle()
-    _sensor = OnlineSensor(_data, _controller)
-    _data["climate_state"] = {"inside_temp": 18.8}
-    _data["charge_state"] = {"charge_rate": 22}
-    _data["vehicle_state"] = {"rt": 0}
-    _data["vehicle_config"] = {"car_type": "model3"}
-    _data["drive_state"] = {"shift_state": "P"}
-    _data["gui_settings"] = {"gui_range_display": "Rated"}
-
-    _controller.set_climate_params(vin=VIN, params=_data["climate_state"])
-    _controller.set_charging_params(vin=VIN, params=_data["charge_state"])
-    _controller.set_state_params(vin=VIN, params=_data["vehicle_state"])
-    _controller.set_config_params(vin=VIN, params=_data["vehicle_config"])
-    _controller.set_drive_params(vin=VIN, params=_data["drive_state"])
-    _controller.set_gui_params(vin=VIN, params=_data["gui_settings"])
-
-    
-    await _sensor.async_update()
-
-    assert _sensor is not None
-    assert _sensor.attrs.get("vehicle_data") is not None
-    assert _sensor.attrs["vehicle_data"] == '{"climate_state": {"inside_temp": 18.8},'\
-                                            ' "charge_state": {"charge_rate": 22},'\
-                                            ' "vehicle_state": {"rt": 0},'\
-                                            ' "vehicle_config": {"car_type": "model3"},'\
-                                            ' "drive_state": {"shift_state": "P"},'\
-                                            ' "gui_settings": {"gui_range_display": "Rated"}}'
-    

tests/unit_tests/homeassistant/test_vehicle_data.py
@@ -1,0 +1,284 @@
+"""Test online sensor."""
+
+import pytest
+import copy
+
+from teslajsonpy.controller import Controller
+from teslajsonpy.homeassistant.vehicle_data import (
+    ChargeStateDataSensor,
+    ClimateStateDataSensor,
+    DriveStateDataSensor,
+    GuiSettingsDataSensor,
+    SoftwareDataSensor,
+    SpeedLimitDataSensor,
+    VehicleConfigDataSensor,
+    VehicleDataSensor,
+    VehicleStateDataSensor,
+)
+
+from tests.tesla_mock import TeslaMock, VIN, CAR_ID
+
+
+def test_dict_to_attr(monkeypatch):
+    """Test converting dict to attributes."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = VehicleDataSensor(_data, _controller)
+
+    data = {
+        "item1": 1,
+        "item2": 2,
+        "dict1": {
+            "item3": 3,
+            "item4": 4,
+            "dict2": {
+                "item5": 5,
+                "dict3": {
+                    "item6": 6,
+                },
+                "dict4": {
+                    "item7": 7,
+                },
+            },
+            "dict5": {"item8": 8},
+        },
+        "dict6": {
+            "item9": 9,
+        },
+        "dict7": {
+            "item10": 10,
+        },
+    }
+
+    attr = {
+        "item1": 1,
+        "item2": 2,
+        "dict1_item3": 3,
+        "dict1_item4": 4,
+        "dict1_dict2_item5": 5,
+        "dict1_dict2_dict3_item6": 6,
+        "dict1_dict5_item8": 8,
+        "dict7_item10": 10,
+    }
+    result = _sensor._dict_to_attr(data, ["dict4", "dict6"])
+    assert result == attr
+
+
+def test_dict_to_attr_no_dicts(monkeypatch):
+    """Test converting dict to attributes."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = VehicleDataSensor(_data, _controller)
+
+    data = {
+        "item1": 1,
+        "item2": 2,
+        "dict1": {
+            "item3": 3,
+            "item4": 4,
+            "dict2": {
+                "item5": 5,
+                "dict3": {
+                    "item6": 6,
+                },
+                "dict4": {
+                    "item7": 7,
+                },
+            },
+            "dict5": {"item8": 8},
+        },
+        "dict6": {
+            "item9": 9,
+        },
+        "dict7": {
+            "item10": 10,
+        },
+    }
+
+    attr = {
+        "item1": 1,
+        "item2": 2,
+    }
+    result = _sensor._dict_to_attr(data, ["*"])
+    assert result == attr
+
+
+@pytest.mark.asyncio
+async def test_charge_state_data_sensor(monkeypatch):
+    """Test charge state data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = ChargeStateDataSensor(_data, _controller)
+    _vehicle_data = "charge_state"
+    _controller.set_charging_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]
+
+
+@pytest.mark.asyncio
+async def test_climate_state_data_sensor(monkeypatch):
+    """Test climate state data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = ClimateStateDataSensor(_data, _controller)
+    _vehicle_data = "climate_state"
+    _controller.set_climate_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]
+
+
+@pytest.mark.asyncio
+async def test_drive_state_data_sensor(monkeypatch):
+    """Test drive state data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = DriveStateDataSensor(_data, _controller)
+    _vehicle_data = "drive_state"
+    _controller.set_drive_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]
+
+
+@pytest.mark.asyncio
+async def test_gui_settings_data_sensor(monkeypatch):
+    """Test gui settings data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = GuiSettingsDataSensor(_data, _controller)
+    _vehicle_data = "gui_settings"
+    _controller.set_gui_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]
+
+
+@pytest.mark.asyncio
+async def test_software_data_sensor(monkeypatch):
+    """Test software data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = SoftwareDataSensor(_data, _controller)
+    _vehicle_data = "vehicle_state"
+    _controller.set_state_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]["software_update"]
+
+
+@pytest.mark.asyncio
+async def test_speed_limit_data_sensor(monkeypatch):
+    """Test speed limit data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = SpeedLimitDataSensor(_data, _controller)
+    _vehicle_data = "vehicle_state"
+    _controller.set_state_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]["speed_limit_mode"]
+
+
+@pytest.mark.asyncio
+async def test_vehicle_config_data_sensor(monkeypatch):
+    """Test vehicle config sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = VehicleConfigDataSensor(_data, _controller)
+    _vehicle_data = "vehicle_config"
+    _controller.set_config_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _data[_vehicle_data]
+
+
+@pytest.mark.asyncio
+async def test_vehicle_state_data_sensor(monkeypatch):
+    """Test vehicle state data sensor."""
+
+    _mock = TeslaMock(monkeypatch)
+    _controller = Controller(None)
+    _controller.set_id_vin(CAR_ID, VIN)
+
+    _data = _mock.data_request_vehicle()
+    _sensor = VehicleStateDataSensor(_data, _controller)
+    _vehicle_data = "vehicle_state"
+    _vehicle_state_data: dict = copy.deepcopy(_data[_vehicle_data])
+
+    _controller.set_state_params(vin=VIN, params=_data[_vehicle_data])
+
+    await _sensor.async_update()
+
+    del _vehicle_state_data["speed_limit_mode"]
+    del _vehicle_state_data["software_update"]
+    del _vehicle_state_data["media_state"]
+    _vehicle_state_data.update({"media_state_remote_control_enabled": True})
+
+    assert _sensor is not None
+    assert _sensor.get_value() == VIN
+    assert _sensor.enabled_by_default is False
+    assert _sensor.attrs == _vehicle_state_data

commit af7019d490bfb5aa71d879a64185c1c0d007682a

style: use typing.List for typehint to support python < 3.9
This fixes a breaking change for users running python 3.8 and lower that was introduced in #299
Type-hinting the contents of a list require importing List from typing on python versions prior to 3.9

teslajsonpy/homeassistant/vehicle_data.py
@@ -7,7 +7,7 @@ https://github.com/zabuldon/teslajsonpy
 """
 import logging
 
-from typing import Optional, Text
+from typing import Optional, Text, List
 from teslajsonpy.homeassistant.vehicle import VehicleDevice
 
 _LOGGER = logging.getLogger(__name__)
@@ -72,7 +72,7 @@ class VehicleDataSensor(VehicleDevice):
     def _dict_to_attr(
         cls,
         data: dict,
-        exclude_dicts: Optional[list[str]] = None,
+        exclude_dicts: Optional[List[str]] = None,
         prepend: Optional[str] = None,
     ) -> dict:
         """Convert Tesla returned dict into dict for attributes."""