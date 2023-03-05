e34126fa8c9b6f3264a77d6c6ed592d0c56a90f8
Alan D. Tse <alandtse@gmail.com>
2023-03-04 23:19:02 -0800

    fix: fix processing or httpx response for orjson

--- teslajsonpy/connection.py
+++ teslajsonpy/connection.py
@@ -208,7 +208,7 @@ class Connection:
                 elif resp.status_code == 408:
                     raise TeslaException(resp.status_code, "vehicle_unavailable")
                 raise TeslaException(resp.status_code)
-            data = orjson.loads(resp)  # pylint: disable=no-member
+            data = orjson.loads(resp.text)  # pylint: disable=no-member
             if data.get("error"):
                 # known errors:
                 #     'vehicle unavailable: {:error=>"vehicle unavailable:"}',
@@ -451,7 +451,7 @@ class Connection:
                     #         }
                     #     ]
                     # }
-                    mfa_json = orjson.loads(mfa_resp)  # pylint: disable=no-member
+                    mfa_json = orjson.loads(mfa_resp.text)  # pylint: disable=no-member
                     if len(mfa_json.get("data", [])) >= 1:
                         factor_id = mfa_json["data"][mfa_device]["id"]
                     if not mfa_code:
@@ -473,7 +473,7 @@ class Connection:
                         },
                     )
                     _process_resp(mfa_resp)
-                    mfa_json = orjson.loads(mfa_resp)  # pylint: disable=no-member
+                    mfa_json = orjson.loads(mfa_resp.text)  # pylint: disable=no-member
                     if not (
                         mfa_json["data"].get("approved")
                         and mfa_json["data"].get("valid")
@@ -535,7 +535,7 @@ class Connection:
             str(self.auth_domain.with_path("/oauth2/v3/token")),
             data=oauth,
         )
-        return orjson.loads(auth)  # pylint: disable=no-member
+        return orjson.loads(auth.text)  # pylint: disable=no-member
 
     async def refresh_access_token(self, refresh_token):
         """Refresh access token from sso."""
@@ -554,7 +554,7 @@ class Connection:
             str(self.auth_domain.with_path("/oauth2/v3/token")),
             data=oauth,
         )
-        return orjson.loads(auth)  # pylint: disable=no-member
+        return orjson.loads(auth.text)  # pylint: disable=no-member
 
     async def get_bearer_token(self, access_token):
         """Get bearer token. This is used by the owners API."""
@@ -574,7 +574,7 @@ class Connection:
         auth = await self.websession.post(
             "https://owner-api.teslamotors.com/oauth/token", headers=head, data=oauth
         )
-        return orjson.loads(auth)  # pylint: disable=no-member
+        return orjson.loads(auth.text)  # pylint: disable=no-member
 
 
 def get_inputs(soup: BeautifulSoup, searchfield=None) -> Dict[str, str]:
